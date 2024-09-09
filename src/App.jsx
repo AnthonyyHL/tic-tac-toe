e17069de4c4880ab47c1060bfa9a1544b4edb290
AnthonyyHL <anthleon@espol.edu.ec>
2024-09-08 23:26:23 -0500

    feat: Adduse states for turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const TURNS = {
+  X: '✖',
+  O: '◯',
+}
+
+const BOARD = Array(9).fill(null)
+
+
+const Cell = ({ children, isSelected, updateCell, index }) => {
+  {console.log(isSelected)}
+  const className = `cell ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateCell(index)
+  }
+
+  return (
+    <button className={className} onClick={ handleClick }>
+      {children}
+    </button>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [board, setBoard] = useState(
+    Array(9).fill(null)
+  )
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateCell = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          BOARD.map((cell, index) => (
+            <Cell
+              key={index}
+              index={index}
+              updateCell={updateCell}
+            >
+              {board[index]}
+            </Cell>
+          ))
+        }
+      </section>
+      <section className="turns">
+        <Cell isSelected={turn === TURNS.X}>{TURNS.X}</Cell>
+        <Cell isSelected={turn === TURNS.O}>{TURNS.O}</Cell>
+      </section>
     </>
   )
 }
