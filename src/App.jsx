d06a95f3d525b277371d48ad7c95f509c3fd786a
AnthonyyHL <anthleon@espol.edu.ec>
2024-09-09 19:32:20 -0500

    feat: Add game winning logic

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ const TURNS = {
 
 const BOARD = Array(9).fill(null)
 
-
 const Cell = ({ children, isSelected, updateCell, index }) => {
-  {console.log(isSelected)}
   const className = `cell ${isSelected ? 'is-selected' : ''}`
 
   const handleClick = () => {
@@ -30,20 +28,53 @@ function App() {
     Array(9).fill(null)
   )
   const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null) // null: no winner | false: draw | true: winner
+
+  const checkWinner = (boardToCheck) => {
+    for (let i = 0; i < 3; i++) {
+      if (
+        boardToCheck[i] !== null &&
+        boardToCheck[i] === boardToCheck[i + 3] &&
+        boardToCheck[i] === boardToCheck[i + 6]
+      ) { return true }
+      else if (
+        boardToCheck[i * 3] !== null &&
+        boardToCheck[i * 3] === boardToCheck[i * 3 + 1] &&
+        boardToCheck[i * 3] === boardToCheck[i * 3 + 2]
+      ) { return true }
+      else if (
+        boardToCheck[i] !== null &&
+        boardToCheck[i] === boardToCheck[i + 4] &&
+        boardToCheck[i] === boardToCheck[i + 8]
+      ) { return true }
+      else if (
+        boardToCheck[i + 2] !== null &&
+        boardToCheck[i + 2] === boardToCheck[i + 4] &&
+        boardToCheck[i + 2] === boardToCheck[i + 6]
+      ) { return true }
+      return null
+    }
+  }
+
 
   const updateCell = (index) => {
-    if (board[index] === null) {
+    if (board[index] === null && !winner) {
       const newBoard = [...board]
       newBoard[index] = turn
       setBoard(newBoard)
   
       const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
       setTurn(newTurn)
+
+      const newWinner = checkWinner(newBoard)
+      if (newWinner) {
+        setWinner(newWinner)
+      }
+
     }
   }
 
   return (
-    console.log('render'),
     <>
       <h1>Tic Tac Toe</h1>
       <section className="game">
