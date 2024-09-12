8aa5bbd11ab03613149dac2ab7faa0e5d7300b97
AnthonyyHL <anthleon@espol.edu.ec>
2024-09-11 22:10:57 -0500

    feat: Add user input cards

--- src/user_input/UsersInput.jsx
+++ src/user_input/UsersInput.jsx
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Cell } from '../components/Cell'
 import { TURNS_ICONS } from '../constants'
 
 import './UsersInput.css'
 
+const PlayerInputCard = ({ player, icon, handlePlayerChange }) => {
+  const playerLabelClassName = icon === TURNS_ICONS.X ? 'player-1' : 'player-2'
+  return (
+    <section className="player-input-card">
+      <label
+        htmlFor={playerLabelClassName}
+        className={`player ${playerLabelClassName}`}
+      >
+        {playerLabelClassName.replace('-', ' ').toLocaleUpperCase()}
+      </label>
+      <Cell>{icon}</Cell>
+      <input
+        id={player}
+        className="player-input"
+        type="text"
+        defaultValue={player}
+        onBlur={(e) => handlePlayerChange(e, icon)}
+      />
+    </section>
+  )
+}
+
 function UsersInput() {
+  const [player1, setPlayer1] = useState('')
+  const [player2, setPlayer2] = useState('')
+
+  const handlePlayerChange = (e, icon) => {
+    const { id, value } = e.target
+    if (icon === TURNS_ICONS.O && id === player1) {
+      alert('Player 2 name already exists')
+      return
+    }
+
+    icon === TURNS_ICONS.X ? setPlayer1(value) : setPlayer2(value)
+  }
+
   return (
     <section className="player-input-container">
-      <section className="player-input-card">
-        <label htmlFor="player1" className="player1">
-          Player 1
-        </label>
-        <Cell>{TURNS_ICONS.X}</Cell>
-        <input type="text" onBlur={(e) => console.log(e.target.value)} />
-      </section>
+      <PlayerInputCard
+        player={player1}
+        icon={TURNS_ICONS.X}
+        handlePlayerChange={handlePlayerChange}
+      />
+      <PlayerInputCard
+        player={player2}
+        icon={TURNS_ICONS.O}
+        handlePlayerChange={handlePlayerChange}
+      />
     </section>
   )
 }
